94f2c0085e12905931bad55e7c83a0610a980034
Sudhakar Jinuka <sjinuka@anergroup.com>
2025-05-22 16:28:40 +0530

    Added file to repo

--- migrate.ps1
+++ migrate.ps1
@@ -5,8 +5,11 @@ $csvFilePath = "/home/sjinuka/sample_data.csv"  # Path to the CSV file on source
 $sshKeyPath = "$home/sjinuka/.ssh/id_rsa.pub"   # Update this path if needed
 $username = "sjinuka"                      # Replace with actual username on VMs
 
-# Step 1: Copy the file from source-vm to your local machine
-scp -i $sshKeyPath "$username@$sourceVMIP:$csvFilePath" "C:\Temp\mydata.csv"
+# Step 1: Copy from source VM to local
+scp -i $sshKeyPath "$username@${sourceVMIP}:${csvFilePath}" "C:\Temp\mydata.csv"
 
-# Step 2: Copy the file from local machine to target-vm
-scp -i $sshKeyPath "C:\Temp\mydata.csv" "$username@$targetVMIP:/home/$username/mydata.csv"
+# Step 2: Copy from local to target VM
+scp -i $sshKeyPath "C:\Temp\mydata.csv" "$username@${targetVMIP}:/home/$username/mydata.csv"
+
+
+
