92b8bc93d14239db3b17b037c0c1e31c986f415b
Sudhakar Jinuka <sjinuka@anergroup.com>
2025-05-22 21:54:03 +0530

    Added file to repo

--- migrate.ps1
+++ migrate.ps1
@@ -10,14 +10,18 @@ Write-Host "Starting migration..."
 Write-Host "Source VM IP: $sourceVMIP"
 Write-Host "Target VM IP: $targetVMIP"
 Write-Host "Username: $username"
-Write-Host "pwd"
 Write-Host "CSV Path on Source VM: $csvFilePath"
 Write-Host "Local CSV Path: $localCsvPath"
 
 # Use ${} to avoid colon parsing issues
 $remoteSource = "${username}@${sourceVMIP}:${csvFilePath}"
 $remoteTarget = "${username}@${targetVMIP}:/home/${username}/mydata.csv"
-$qey = "pwd"
+Write-Host "SSh connection test"
+ssh -o StrictHostKeyChecking=no "${username}@${sourceVMIP}" "pwd"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Failed to ssh connection test from source VM."
+    exit 1
+}
 
 Write-Host "Copying from source VM..."
 scp -o StrictHostKeyChecking=no "$remoteSource" "$localCsvPath"
